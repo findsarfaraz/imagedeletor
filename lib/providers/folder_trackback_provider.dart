191f0ba7a54706c20d8cc36f6b8e15d781a7d602
findsarfaraz <findsarfaraz@gmail.com>
2022-01-06 19:58:30 +0530

    Added provider to track path

--- lib/providers/folder_trackback_provider.dart
+++ lib/providers/folder_trackback_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:imagedeletor/model/folder_trackback_model.dart';
+import 'package:imagedeletor/notifiers/fold_trackback_notifier.dart';
 
 final folderTrackBackProvider =
-    ChangeNotifierProvider<FolderTrackBackData>((ref) {
-  return FolderTrackBackData();
+    ChangeNotifierProvider<FolderTrackBackNotifier>((ref) {
+  return FolderTrackBackNotifier();
 });
